d563c6cea4131dc89190c1c033a3166c4d8f9a22
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-15 00:00:05 +0700

    [ADD] Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Post extends Model
 {
     //
+    use HasFactory, Notifiable;
 
     // public static function all()
     // {
