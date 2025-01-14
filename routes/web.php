7669eabb09647680e41c2b95766b49aed68a8886
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-14 18:44:56 +0700

    [ADD] View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,8 +12,51 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Ultimate Attack', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Sauqisan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam cum aliquid inventore? kokoaki ainaisakdajosdjausau'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Sauqisan',
+            'body' => 'Lorem, ipsum dolor.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    // dd($id);
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Sauqisan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam cum aliquid inventore? kokoaki ainaisakdajosdjausau'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Sauqisan',
+            'body' => 'Lorem, ipsum dolor.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    // dd($post);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
