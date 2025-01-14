c27f24ee5829ff3aed1b5c758293f7d974ac37de
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-14 16:33:35 +0700

    [ADD] Blade Component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Blog</h1>
-    <article>
-        <h3>Judul 1</h3>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur qui voluptates explicabo!
-    </article>
-    <article>
-        <h3>Judul 2</h3>
-        Lorem ipsum dolor sit amet.
-    </article>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini adalah halaman Blog</h3>
+</x-layout>
